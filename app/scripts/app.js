bc4f4b04bd2495842ee5ed23ac0ee17ebe15dc22
Santiago Petrone <arthos4g@gmail.com>
2016-01-16 18:10:05 -0300

    Update Main -> Dashboard controller. Routes added. Translate setup

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,35 +1,89 @@
-'use strict';
+(function() {
+  'use strict';
 
-/**
- * @ngdoc overview
- * @name kcmDashboardApp
- * @description
- * # kcmDashboardApp
- *
- * Main module of the application.
- */
-angular
-  .module('kcmDashboardApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+  /**
+   * @ngdoc overview
+   * @name kcmDashboardApp
+   * @description
+   * # kcmDashboardApp
+   *
+   * Main module of the application.
+   */
+  angular
+    .module('kcmDashboardApp', [
+      'ngAnimate',
+      'ngCookies',
+      'ngResource',
+      'ngRoute',
+      'ngSanitize',
+      'ngTouch',
+      'pascalprecht.translate'
+    ])
+    .config(configuration);
+
+    configuration.$inject = ['$routeProvider', '$translateProvider'];
+
+    function configuration($routeProvider, $translateProvider) {
+
+      configureRouter();
+      configureTranslate();
+
+      function configureRouter() {
+        $routeProvider
+          .when('/', {
+            templateUrl: 'views/dashboard.html',
+            controller: 'DashboardCtrl',
+            controllerAs: 'dashboard'
+          })
+          .when('/human-resources', {
+            templateUrl: 'views/human-resources.html',
+            controller: 'HumanResourcesCtrl',
+            controllerAs: 'humanResources'
+          })
+          .when('/finance', {
+            templateUrl: 'views/finance.html',
+            controller: 'FinanceCtrl',
+            controllerAs: 'Finance'
+          })
+          .when('/reporting', {
+            templateUrl: 'views/reporting.html',
+            controller: 'ReportingCtrl',
+            controllerAs: 'reporting'
+          })
+          .when('/support', {
+            templateUrl: 'views/support.html',
+            controller: 'SupportCtrl',
+            controllerAs: 'support'
+          })
+          .otherwise({
+            redirectTo: '/'
+          });
+      }
+
+      function configureTranslate() {
+        $translateProvider.translations('en', {
+          'dashboard': {
+            'options': {
+              'dashboard': 'Dashboard',
+              'finance': 'Finance',
+              'reporting': 'Reporting',
+              'humanResources': 'Human Resources',
+              'support': 'Support'
+            }
+          },
+          'header': {
+            'search': {
+              'placeholder': 'Search...',
+              'button': 'Go!'
+            }
+          },
+          'footer': {
+            'copyright': 'KCM Automotors - 2016'
+          }
+        });
+
+        $translateProvider.preferredLanguage('en');
+        $translateProvider.useSanitizeValueStrategy('escaped');
+      }
+    }
+})();
--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -8,7 +8,7 @@
  * Controller of the kcmDashboardApp
  */
 angular.module('kcmDashboardApp')
-  .controller('MainCtrl', function () {
+  .controller('DashboardCtrl', function () {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
